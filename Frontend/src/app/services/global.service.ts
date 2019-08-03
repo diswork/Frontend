e7d705c5b3f3ce5aac31e08e3646f357d93dea1d
Krowser1809 <gabrielcaba1809@gmail.com>
2019-08-03 16:52:05 -0600

    servicio global alterado

--- Frontend/src/app/services/global.service.ts
+++ Frontend/src/app/services/global.service.ts
@@ -1,4 +1,5 @@
 export var GLOBAL = {
-  url: 'http://localhost:3000/api/'
+  //url: 'http://localhost:3000/api/'
   //url: 'http://192.168.8.101:3000/api/'
+  url: 'http://192.168.56.1:3000/api/'
 }
